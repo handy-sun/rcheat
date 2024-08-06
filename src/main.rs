1405b5d3baeecf54a73cc38d7cca6e65c8751713
sooncheer <handy-sun@foxmail.com>
2024-08-06 14:49:06 +0800

    ptrace::attach->wait::waitpid->ptrace::detach

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,49 @@
-use libc::pid_t;
-use std::env;
-use std::process;
+use std::{env, process};
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let sys_args = env::args();
-    let mut pid: pid_t = 0;
-    for (i, arg) in sys_args.enumerate() {
-        if i == 1 {
-            pid = arg.parse().unwrap();
-        }
-    }
+use nix::sys::ptrace;
+use nix::sys::wait;
+use nix::unistd::Pid;
+use nix::libc::pid_t;
 
-    println!("My pid is: {}", unsafe { libc::getpid() });
-
-    if let 0 = pid {
-        eprintln!("Target pid is zero!");
-        process::exit(1);
-    }
-
-    let result = unsafe { libc::ptrace(libc::PTRACE_SEIZE, pid, 0, 0) };
-    match result >= 0 {
-        true => {
-            println!("ptrace seize succeeded");
-            return Ok(());
-        }
-        false => {
+fn print_or_exit(ret: &nix::Result<()>, msg: &str) {
+    match ret {
+        Ok(_) => println!("{} succeeded", msg),
+        Err(e) => {
             let os_err = std::io::Error::last_os_error();
-            eprintln!("result={}, {:?}", result, os_err);
+            eprintln!("{} failed: {:?}, {:?}", msg, e, os_err);
             process::exit(os_err.raw_os_error().unwrap_or(-1));
         }
     }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let arg_vec: Vec<String> = env::args().collect();
+    if arg_vec.len() < 2 {
+        eprintln!("please input pid!");
+        process::exit(1);
+    }
+
+    let raw_pid: pid_t = arg_vec.get(1).unwrap_or(&String::from("0")).parse()?;
+    if raw_pid < 2 {
+        eprintln!("the pid must greater than 1!");
+        process::exit(1);
+    }
+    let pid = Pid::from_raw(raw_pid);
+
+    println!("self pid is: {:?}", nix::unistd::getpid());
+
+    print_or_exit(&ptrace::attach(pid), "ptrace attach");
+
+    match wait::waitpid(pid, None) {
+        Ok(status) => println!("waitpid succeeded, status: {:?}", status),
+        Err(e) => {
+            let os_err = std::io::Error::last_os_error();
+            eprintln!("waitpid failed: {:?}, {:?}", e, os_err);
+            process::exit(os_err.raw_os_error().unwrap_or(-1));
+        }
+    }
+
+    print_or_exit(&ptrace::detach(pid, None), "ptrace detach");
+
+    Ok(())
 }
