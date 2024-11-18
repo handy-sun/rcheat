4be0c255c8a8f9a93951c7fe04ed05b6e5052520
sooncheer <handy-sun@foxmail.com>
2024-11-18 17:46:22 +0800

    SetupTableData create 2 levels table; update mlua version

--- lua/core.lua
+++ lua/core.lua
@@ -1,21 +1,23 @@
 function SetupTableData(bytes, tab_list)
-    local index = 1
-    local new_list = {}
-    while true do
-        for _, value in pairs(tab_list) do
-        if index + value.size - 1 > #bytes then
-            return new_list
-        end
-        local next_index = index + value.size
-        local part_bytes = string.sub(bytes, index, next_index - 1)
+	local index = 1
+	local new_list = {}
+	while true do
+		local one_piece = {}
+		for _, value in pairs(tab_list) do
+			if index + value.size - 1 > #bytes then
+				return new_list
+			end
+			local next_index = index + value.size
+			local part_bytes = string.sub(bytes, index, next_index - 1)
 
-        table.insert(new_list, {
-            name = value.name,
-            size = value.size,
-            data = string.unpack(value.fmt, part_bytes)
-        })
+			table.insert(one_piece, {
+				name = value.name,
+				size = value.size,
+				data = string.unpack(value.fmt, part_bytes)
+			})
 
-        index = next_index
-        end
-    end
+			index = next_index
+		end
+		table.insert(new_list, one_piece)
+	end
 end
