a96a629227997112ff473e0f89495ba309921a3f
sooncheer <handy-sun@foxmail.com>
2024-12-25 23:58:14 +0800

    update some crates and shadow_rs' deny

--- build.rs
+++ build.rs
@@ -4,5 +4,7 @@ fn main() -> shadow_rs::SdResult<()> {
     let mut deny = BTreeSet::new();
     deny.insert(shadow_rs::CARGO_TREE);
     deny.insert(shadow_rs::CARGO_METADATA);
-    shadow_rs::new_deny(deny)
+    // shadow_rs::new_deny(deny)
+    shadow_rs::ShadowBuilder::builder().deny_const(deny).build()?;
+    Ok(())
 }
